822672967d077702b83f48f73db3ad4ed2ba18c8
Nabil <nabil.noble01@gmail.com>
2022-11-12 05:45:16 +0000

    use utcnow for all datatime and fix call of super()

--- models/base_model.py
+++ models/base_model.py
@@ -26,12 +26,12 @@ class BaseModel:
             kwargs['updated_at'] = datetime.strptime(kwargs['updted_at'], %Y-%m-%dT%H:%M:%S.%f)
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            self.created_at = datetime.utcnow()
+            self.updated_at = datetime.utcnow()
 
     def save(self):
         """updates updated_at attribute"""
-        self.updated_at = datetime.now()
+        self.updated_at = datetime.utcnow()
         models.database_storage.new(self)
         models.database_storage.save()
 
--- models/user.py
+++ models/user.py
@@ -19,7 +19,7 @@ class User(BaseModel, Base):
     password = Column(String(200), nullable=False)
     phone_number = Column(String(100), nullable=False)
     first_name = Column(String(100), nullable=False)
-    last_name = Column(String(100), nullable=False
+    last_name = Column(String(100), nullable=False)
     gender = Column(String(100), nullable=False)
     birthday = Column(String(100), nullable=False)
 
@@ -28,11 +28,11 @@ class User(BaseModel, Base):
 
     def __init__(self, *args, **kwargs):
         """initialization"""
-        super().__init__(self, *args, **kwargs)
+        super().__init__(*args, **kwargs)
 
     def __setattr__(self, name, value):
         """sets password attributes with hash function algorithm SHA-2"""
         if name == "password":
             value = hashlib.sha512(value.encode()).hexdigest()
 
-        super().__setattr__(self, name, value)
+        super().__setattr__(name, value)
