e5401e032b89671bffa5f1dda6eed4d6840f21bf
Nabil <nabil.noble01@gmail.com>
2022-11-12 19:05:10 +0000

    wrapping models

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -3,8 +3,24 @@
 from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import sessionmaker, scoped_session
 import os
-from models.base_model import Base
+from models.base_model import BaseModel, Base
 from models.user import User
+from models.patient import Patient
+from models.doctor import Doctor
+from models.specialization import Specialization
+from models.review import Review
+from models.hospital_affiliation import HospitalAffiliation
+from models.office import Office
+from models.office_hours import OfficeHours
+from models.appointment import Appointment
+from models.appointment_status import AppointmentStatus
+
+classes = {"Patient": Patient, "Doctor": Doctor,
+           "Review": Review, "Specialization": Specialization,
+           "User": User, "HospitalAffiliation": HospitalAffiliation,
+           "Office": Office, "OfficeHours": OfficeHours,
+           "Appointment": Appointment,
+           "AppointmentStatus": AppointmentStatus}
 
 
 class DBstorage:
@@ -19,7 +35,18 @@ class DBstorage:
             os.environ.get('ARC_MYSQL_USER'), os.environ.get('ARC_MYSQL_PASSWORD'),
             os.environ.get('ARC_MYSQL_HOST'), os.environ.get('ARC_MYSQL_DB')))
 
-    def all(self):
+    def all(self, cls=None):
+        """query the current database session and return objects
+        depending on the class name"""
+        dictionary = {}
+        for classes in classes:
+            if cls is None or cls is classes[classe] or cls is classe:
+                objs = self.__session.query(classes[classe]).all()
+                for obj in objs:
+                    key = obj.__class__.__name__ + '.' + obj.id
+                    dictionary[key] = obj
+        return dictionary
+                
 
     def new(self, obj):
         """brand new instancs added"""
