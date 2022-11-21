a54c028bcb0cf6d6a03f2ea4411821f5d31b9c81
ImzyO <100032916+ImzyO@users.noreply.github.com>
2022-11-21 12:24:11 +0000

    api

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -6,6 +6,8 @@ from api.v1.views.index import *
 from api.v1.views.users import *
 from api.v1.views.patients import *
 from api.v1.views.doctors import *
+from api.v1.views.specializations import *
+
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
